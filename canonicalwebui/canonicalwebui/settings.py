80e973966223f9695ff29907e9e4dc8d7e2686ce
Depanshu Singh <depanshuiit2017@gmail.com>
2025-05-21 22:34:28 +0530

    Added VacoLogo

--- canonicalwebui/canonicalwebui/settings.py
+++ canonicalwebui/canonicalwebui/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'core',  # Custom app for core functionality
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
